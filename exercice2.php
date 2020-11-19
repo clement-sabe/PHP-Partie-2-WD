f4fa317e7ba797ff1a124a9b48ce5f68ee3f5b98
clément-sabe <clem.sabe@gmail.com>
2020-11-19 17:36:35 +0100

    go

--- exercice2.php
+++ exercice2.php
@@ -5,20 +5,20 @@ include 'header.php';
 ?>
 
 <p class="text-center h2">Vous êtes <?php
-    if ($age >= 18 && $age <=120 && $gender == 'Homme'){
+    if ($age >= 18 && $age <=120 && $gender == 'Homme'){// age supérieur ou egale a 18 et un homme
         echo ' un homme et vous êtes majeur';
     }
-    elseif ($age < 18 && $age<=0 && $gender == 'Homme'){
+    elseif ($age < 18 && $age<=0 && $gender == 'Homme'){//age inférieur ou egale a 18 et un homme
         echo ' un homme et vous êtes mineur';
     }
-    elseif ($age >= 18 && $age <=120 && $gender == 'Femme'){
+    elseif ($age >= 18 && $age <=120 && $gender == 'Femme'){// age supérieur ou egale a 18 et un femme
         echo ' une femme et vous êtes majeure';
     }
-    elseif ($age < 18 && $age<=0 && $gender == 'Femme'){
+    elseif ($age < 18 && $age<=0 && $gender == 'Femme'){//age inférieur ou egale a 18 et un femme
         echo ' une femme et vous êtes mineure';
     }
     else{
-        echo ' chelou!';
+        echo ' chelou !';
     }?>
 </p>
 
