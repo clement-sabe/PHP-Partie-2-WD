62c57a59c238abfa4a4a21d4cfdb2395142b54e7
clément-sabe <clem.sabe@gmail.com>
2020-11-19 15:00:01 +0100

    go

--- exercice1.php
+++ exercice1.php
@@ -4,11 +4,13 @@ include 'header.php';
 ?>
 
     <p class="text-center h2">Vous êtes <?php
-    if ($age >= 18){
+    if ($age >= 18 && $age <= 120){
         echo 'majeur';
     }
-    elseif ($age < 18){
+    elseif ($age >= 0 && $age <= 17  ){
         echo 'mineur';
+    }else{
+        echo 'chelou!';
     }?>
     </p>
 <?php include 'footer.php';
--- exercice2.php
+++ exercice2.php
@@ -1,24 +1,24 @@
 <?php
-  $age = 32;
+  $age = 122;
 $gender = 'Homme';
 include 'header.php';
 ?>
 
 <p class="text-center h2">Vous êtes <?php
-    if ($age >= 18 && $gender == 'Homme'){
+    if ($age >= 18 && $age <=120 && $gender == 'Homme'){
         echo ' un homme et vous êtes majeur';
     }
-    elseif ($age < 18 && $gender == 'Homme'){
+    elseif ($age < 18 && $age<=0 && $gender == 'Homme'){
         echo ' un homme et vous êtes mineur';
     }
-    elseif ($age >= 18 && $gender == 'Femme'){
+    elseif ($age >= 18 && $age <=120 && $gender == 'Femme'){
         echo ' une femme et vous êtes majeure';
     }
-    elseif ($age < 18 && $gender == 'Femme'){
+    elseif ($age < 18 && $age<=0 && $gender == 'Femme'){
         echo ' une femme et vous êtes mineure';
     }
     else{
-        echo ' pas humain!';
+        echo ' chelou!';
     }?>
 </p>
 
