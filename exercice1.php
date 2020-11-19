80a3e3f946bbb6801fd93853ed326846104de05b
clément-sabe <clem.sabe@gmail.com>
2020-11-19 15:02:57 +0100

    go

--- exercice1.php
+++ exercice1.php
@@ -4,12 +4,12 @@ include 'header.php';
 ?>
 
     <p class="text-center h2">Vous êtes <?php
-    if ($age >= 18 && $age <= 120){
+    if ($age >= 18 && $age <= 120){ //age compris entre 18 et 120
         echo 'majeur';
     }
-    elseif ($age >= 0 && $age <= 17  ){
+    elseif ($age >= 0 && $age <= 17  ){ //age compris entre 0 et 17
         echo 'mineur';
-    }else{
+    }else{// age inferieur a 0 ou supérieur a 120
         echo 'chelou!';
     }?>
     </p>
